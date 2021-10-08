7e4dbb903de7bc2068478bbff7903124463f0bb3
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2021-10-09 00:17:04 +0100

    tidy up

--- src/shared/messages/Message.ts
+++ src/shared/messages/Message.ts
@@ -11,7 +11,7 @@ import SCHEMA, { LITTLE_ENDIAN, MESSAGE_TYPE, FIELD_TYPES } from "./schema";
 // TODO: jests
 class Message {
   private _fieldDecoders: {
-    [key: string]: (currentOffset: number, binaryMessageView: DataView) => any[];
+    [key: string]: (currentByteOffset: number, binaryMessageView: DataView) => any[];
   };
 
   constructor() {
@@ -28,13 +28,16 @@ class Message {
 
   parseBinary = (binaryMessage: ArrayBuffer) => {
     const binaryMessageView = new DataView(binaryMessage);
-    let [messageType, currentOffset] = this.parseUInt8(MESSAGE_TYPE, binaryMessageView);
+    let [messageType, currentByteOffset] = this.parseUInt8(MESSAGE_TYPE, binaryMessageView);
 
     const messageObject = { messageType };
     SCHEMA[messageType].binary.forEach(([fieldName, fieldType]) => {
-      const [data, nextOffset] = this._fieldDecoders[fieldType](currentOffset, binaryMessageView);
+      const [data, nextByteOffset] = this._fieldDecoders[fieldType](
+        currentByteOffset,
+        binaryMessageView
+      );
       messageObject[fieldName] = data;
-      currentOffset = nextOffset;
+      currentByteOffset = nextByteOffset;
     });
     return messageObject;
   };
@@ -57,54 +60,54 @@ class Message {
   // toBinary = (parsedMessage) => {};
   // componentToBinary = (messageComponent) => {};
 
-  private parseUInt8 = (currentOffset: number, binaryMessageView: DataView) => {
-    const data = binaryMessageView.getUint8(currentOffset);
-    return [data, currentOffset + 1];
+  private parseUInt8 = (currentByteOffset: number, binaryMessageView: DataView) => {
+    const data = binaryMessageView.getUint8(currentByteOffset);
+    return [data, currentByteOffset + 1];
   };
 
-  private parseUInt16 = (currentOffset: number, binaryMessageView: DataView) => {
-    const data = binaryMessageView.getUint16(currentOffset, LITTLE_ENDIAN);
-    return [data, currentOffset + 2];
+  private parseUInt16 = (currentByteOffset: number, binaryMessageView: DataView) => {
+    const data = binaryMessageView.getUint16(currentByteOffset, LITTLE_ENDIAN);
+    return [data, currentByteOffset + 2];
   };
 
-  private parseInt32 = (currentOffset: number, binaryMessageView: DataView) => {
-    const data = binaryMessageView.getInt32(currentOffset, LITTLE_ENDIAN);
-    return [data, currentOffset + 4];
+  private parseInt32 = (currentByteOffset: number, binaryMessageView: DataView) => {
+    const data = binaryMessageView.getInt32(currentByteOffset, LITTLE_ENDIAN);
+    return [data, currentByteOffset + 4];
   };
 
-  private parseFloat32 = (currentOffset: number, binaryMessageView: DataView) => {
-    const data = binaryMessageView.getFloat32(currentOffset, LITTLE_ENDIAN);
-    return [data, currentOffset + 4];
+  private parseFloat32 = (currentByteOffset: number, binaryMessageView: DataView) => {
+    const data = binaryMessageView.getFloat32(currentByteOffset, LITTLE_ENDIAN);
+    return [data, currentByteOffset + 4];
   };
 
-  private parseString = (currentOffset: number, binaryMessageView: DataView) => {
-    if (SERVER) return this.serverParseString(currentOffset, binaryMessageView);
-    return this.clientParseString(currentOffset, binaryMessageView);
+  private parseString = (currentByteOffset: number, binaryMessageView: DataView) => {
+    if (SERVER) return this.serverParseString(currentByteOffset, binaryMessageView);
+    return this.clientParseString(currentByteOffset, binaryMessageView);
   };
 
-  private serverParseString = (currentOffset: number, { buffer: arrayBuffer }) => {
-    const textSlice = arrayBuffer.slice(currentOffset, arrayBuffer.byteLength);
+  private serverParseString = (currentByteOffset: number, { buffer: arrayBuffer }) => {
+    const textSlice = arrayBuffer.slice(currentByteOffset, arrayBuffer.byteLength);
     const data = Buffer.from(textSlice).toString("utf-8");
 
     // NOTE: next offset not super useful here as strings will be the last component in any message
     // since they're final size is unknown.
-    return [data, currentOffset + textSlice.byteLength];
+    return [data, currentByteOffset + textSlice.byteLength];
   };
 
-  private clientParseString = (currentOffset: number, { buffer: arrayBuffer }) => {
-    const textSlice = arrayBuffer.slice(currentOffset, arrayBuffer.byteLength);
+  private clientParseString = (currentByteOffset: number, { buffer: arrayBuffer }) => {
+    const textSlice = arrayBuffer.slice(currentByteOffset, arrayBuffer.byteLength);
     // @ts-ignore
     const data = new TextDecoder("utf-8").decode(textSlice);
 
     // NOTE: next offset not super useful here as strings will be the last component in any message
     // since they're final size is unknown.
-    return [data, currentOffset + textSlice.byteLength];
+    return [data, currentByteOffset + textSlice.byteLength];
   };
 
-  private parseUInt16Array = (currentOffset: number, { buffer: arrayBuffer }) => {
+  private parseUInt16Array = (currentByteOffset: number, { buffer: arrayBuffer }) => {
     // NOTE: not using Uint16Array because it only uses platform default byte order, while I want to
     // force little endian for consistency!
-    const arrayBinaryView = new DataView(arrayBuffer, currentOffset);
+    const arrayBinaryView = new DataView(arrayBuffer, currentByteOffset);
     let data: number[] = [];
     let currentArrayOffset = 0;
 
@@ -116,7 +119,7 @@ class Message {
 
     // NOTE: next offset not super useful here as array will be the last component in any message
     // since its final size is unknown.
-    return [data, currentOffset + arrayBinaryView.byteLength];
+    return [data, currentByteOffset + arrayBinaryView.byteLength];
   };
 }
 
