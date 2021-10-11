ab6086d94fe645dcfbcde2ec09f236c837cd951b
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2021-10-11 19:57:14 +0100

    character move system ready

--- src/server/Game.ts
+++ src/server/Game.ts
@@ -16,6 +16,7 @@ import Movement from "./systems/Movement";
 import State from "./game/State";
 import SpatialPartitioning from "./systems/SpatialPartitioning";
 import CharacterConnected from "./systems/CharacterConnected";
+import CharacterMove from "./systems/CharacterMove";
 
 class Game {
   lastDeltaTime: any;
@@ -50,8 +51,8 @@ class Game {
     this._engine.addSystem(new Movement(this._engine));
     // this._engine.addSystem(new Collision(this._engine)); // TODO: takes in transform and checks it against map. Might be useful to store 'previous' values on Transform (that get auto updated) so in case of collision Transform could be reverted to that?
     this._engine.addSystem(new SpatialPartitioning(this._engine, this._state));
-    this._engine.addSystem(new CharacterConnected(this._engine, this._state)); // TODO
-    this._engine.addSystem(new CharacterMove(this._engine)); // TODO:
+    this._engine.addSystem(new CharacterConnected(this._engine, this._state));
+    this._engine.addSystem(new CharacterMove(this._engine));
     // TODO: any other systems here
     // this._engine.addSystem(new Serializer(this._engine)); # gonna invoke sidekiq workers
     // this._engine.addSystem(new AI(this._engine));
---
+++ src/server/systems/CharacterMove.ts
@@ -0,0 +1,50 @@
+import Move from "../../shared/components/message/Move";
+import Transform from "../../shared/components/Transform";
+import { Engine } from "../../shared/ecs";
+import System from "../../shared/ecs/System";
+import { EntityId } from "../../shared/ecs/types";
+import { SparseSetItem } from "../../shared/ecs/utils/SparseSet";
+import NearbyCharacters from "../components/NearbyCharacters";
+import OutgoingMessage from "../components/OutgoingMessage";
+
+class CharacterMove extends System {
+  constructor(engine: Engine) {
+    super(engine);
+  }
+
+  start(): void {}
+
+  update(): void {
+    this.engine.query(this.createServerMessages, Move);
+  }
+
+  destroy(): void {}
+
+  private createServerMessages = ([move]: [Move]) => {
+    let serverMessageComponents: OutgoingMessage[] = [];
+
+    // TODO: use future 'Entity' api here...
+    const nearbyCharacters = this.engine.getComponent<NearbyCharacters>(NearbyCharacters, move.id);
+    const transform = this.engine.getComponent<Transform>(Transform, move.id);
+
+    serverMessageComponents.push(this.createPositionMessageComponent(transform, transform.id));
+
+    nearbyCharacters.entityIdsSet.stream(({ id: nearbyCharacterEntityId }: SparseSetItem) => {
+      serverMessageComponents.push(
+        this.createPositionMessageComponent(transform, nearbyCharacterEntityId)
+      );
+    });
+
+    this.engine.addComponents(...serverMessageComponents);
+  };
+
+  private createPositionMessageComponent = (
+    { position, id: characterId }: Transform,
+    toEntityId: EntityId
+  ) => {
+    const parsedMessage = { characterId, ...position.xyz };
+    return new OutgoingMessage(this.engine.generateEntityId(), parsedMessage, toEntityId);
+  };
+}
+
+export default CharacterMove;
