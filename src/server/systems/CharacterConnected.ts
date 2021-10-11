206fdcecdc384d3c6c20afca5ba1ac35e978fc14
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2021-10-11 19:36:43 +0100

    characger connected system ready

--- src/server/systems/CharacterConnected.ts
+++ src/server/systems/CharacterConnected.ts
@@ -38,6 +38,7 @@ class CharacterConnected extends System {
 
     let serverMessageComponents: OutgoingMessage[] = [];
     const newCharacterEntityId = connectionEvent.id;
+
     const messagesToInformCharacterOfSelf = this.messagesToInformNewCharacterOfSelf(
       room,
       name,
@@ -75,7 +76,7 @@ class CharacterConnected extends System {
 
   private messagesToInformNewCharacterOfSelf = (room: Room, name: Name, transform: Transform) => {
     return [
-      this.createRoomInitMessageComponent(room),
+      this.createRoomInitMessageComponent(room, room.id),
       this.createConnectedMessageComponent(name, name.id),
       this.createPositionMessageComponent(transform, transform.id),
     ];
@@ -92,7 +93,7 @@ class CharacterConnected extends System {
     ];
   };
 
-  private createRoomInitMessageComponent = ({ roomName, id: toEntityId }: Room) => {
+  private createRoomInitMessageComponent = ({ roomName }: Room, toEntityId: EntityId) => {
     const { tileSizeInPx, widthInTiles, heightInTiles, tiles } = this._state.rooms[roomName];
     const parsedMessage = { tileSizeInPx, widthInTiles, heightInTiles, tiles };
     return new OutgoingMessage(this.engine.generateEntityId(), parsedMessage, toEntityId);
@@ -116,53 +117,3 @@ class CharacterConnected extends System {
 }
 
 export default CharacterConnected;
-
-//     private def handle_character_enter(query_set)
-//       connection_event, name, transform, location, nearby_characters = Tuple(Component::ConnectionEvent, Component::Name, Component::Transform, Component::Location, Component::NearbyCharacters).from(query_set)
-//       character_entity_id = connection_event.id
-
-//       components = [] of Component::ServerMessage
-
-//       # messages to inform client of client
-//       components.push(map_init_message(location.current_map_name, character_entity_id))
-//       components.push(enter_message(character_entity_id, name, character_entity_id))
-//       components.push(position_message(character_entity_id, transform, character_entity_id))
-
-//       nearby_characters.entity_ids_set.stream do |entity_id_item|
-//         nearby_character_entity_id = entity_id_item.id
-//         # messages to inform others of client
-//         components.push(enter_message(character_entity_id, name, nearby_character_entity_id))
-//         components.push(position_message(character_entity_id, transform, nearby_character_entity_id))
-
-//         # messages to inform client of others
-//         nearby_character_name = engine.get_component(Component::Name, nearby_character_entity_id).as Component::Name
-//         nearby_character_transform = engine.get_component(Component::Transform, nearby_character_entity_id).as Component::Transform
-
-//         components.push(enter_message(nearby_character_entity_id, nearby_character_name, character_entity_id))
-//         components.push(position_message(nearby_character_entity_id, nearby_character_transform, character_entity_id))
-//       end
-
-//       engine.add_components(components)
-//     end
-
-//     private def enter_message(subject_character_entity_id, name, recipient_character_entity_id)
-//       Component::ServerMessage.new(
-//         entity_id: engine.generate_entity_id,
-//         to_entity_id: recipient_character_entity_id,
-//         message: Pulse::Messages::Enter.new(subject_character_entity_id, name.name)
-//       )
-//     end
-
-//     private def position_message(subject_character_entity_id, transform, recipient_character_entity_id)
-//       message = Pulse::Messages::Position.new(
-//         subject_character_entity_id, transform.position.x, transform.position.y
-//       )
-
-//       Component::ServerMessage.new(
-//         entity_id: engine.generate_entity_id,
-//         to_entity_id: recipient_character_entity_id,
-//         message: message
-//       )
-//     end
-//   end
-// end
