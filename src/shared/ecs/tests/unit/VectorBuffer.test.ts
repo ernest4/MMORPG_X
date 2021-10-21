6112ccd9ac98802ce8e0933ef4383e528cc6ac8e
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2021-10-21 23:46:02 +0100

    spec groundwork

---
+++ src/shared/ecs/tests/unit/VectorBuffer.test.ts
@@ -0,0 +1,28 @@
+import { context } from "../../../../../tests/jestHelpers";
+import VectorBuffer from "../../utils/VectorBuffer";
+
+describe(VectorBuffer, () => {
+  // context("when argument is number", () => {
+  //   it("returns true", () => {
+  //     expect(isNumber(5)).toEqual(true);
+  //     expect(isNumber(5.89)).toEqual(true);
+  //   });
+  // });
+  // context("when argument is not a number", () => {
+  //   it("returns true", () => {
+  //     expect(isNumber("6")).toEqual(false);
+  //     expect(isNumber("6.78")).toEqual(false);
+  //     expect(isNumber(null)).toEqual(false);
+  //     expect(isNumber(undefined)).toEqual(false);
+  //     expect(isNumber({})).toEqual(false);
+  //     expect(isNumber([])).toEqual(false);
+  //     expect(isNumber(() => {})).toEqual(false);
+  //   });
+  // });
+});
+
+// BENCHMARK
+// const t0 = performance.now();
+// doSomething();
+// const t1 = performance.now();
+// console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
